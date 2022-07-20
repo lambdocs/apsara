bb9848d4b3cc50950f9ea118ab9fe62a749b8044
Ajay Mendez <ajay.mendez@gmail.com>
2022-07-20 09:37:17 +0530

    Group id and GPG identity in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject apsara "0.1.0"
+(defproject net.clojars.ajaym/apsara "0.1.0"
   :description "Editor macros in Clojure"
   :url "https://github.com/lambdocs/apsara"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :signing {:gpg-key "ajay.mendez@gmail.com"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/clojurescript "1.10.896" :scope "provided"]
                  [reagent "1.1.0"]
